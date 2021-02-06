81511bc06c2fe9de937a1dd89f7bc395cf996e1f
karukan013 <karukan029@gmail.com>
2021-02-06 23:21:49 +0900

    Add Preview
    Change url

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -18,7 +18,7 @@ export const getStaticPaths = async () => {
     const key = {
       headers: {'X-API-KEY': process.env.API_KEY},
     };
-    const data = await fetch(`${process.env.END_POINT}/blog`, key)
+    const data = await fetch('https://kazu013.microcms.io/api/v1/blog', key)
       .then(res => res.json())
       .catch(() => null);
     const paths = data.contents.map(content => `/blog/${content.id}`);
@@ -31,12 +31,12 @@ export const getStaticPaths = async () => {
     const key = {
       headers: {'X-API-KEY': process.env.API_KEY},
     };
-    let url = `${process.env.END_POINT}/blog/${id}`;
+    let url = `https://kazu013.microcms.io/api/v1/blog/${id}`;
     if (context.preview){
       url += `?draftKey=${context.previewData.draftKey}`; 
     }
     const data = await fetch(
-      `${process.env.END_POINT}/blog/${id}`, key
+      `https://kazu013.microcms.io/api/v1/blog/${id}`, key
     )
     .then(res => res.json())
     .catch(() => null);
--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export const getStaticProps = async () => {
   const key = {
     headers:{'X-API-KEY':process.env.API_KEY},
   };
-  const data = await fetch(`${process.env.END_POINT}/blog`,key)
+  const data = await fetch('https://kazu013.microcms.io/api/v1/blog',key)
   .then(res => res.json())
   .catch(() => null);
   return {
