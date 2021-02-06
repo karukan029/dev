efbb173dcaf4714ec0e1513ed6d206dc53fa4c33
karukan013 <karukan029@gmail.com>
2021-02-06 21:49:14 +0900

    あっ

---
+++ pages/[slug].jsx
@@ -0,0 +1,16 @@
+export const getStaticProps = async (context) => {
+  const slug = context.params?.slug;
+  const draftKey = context.previewData?.draftKey;
+  const content = await fetch(
+    `https://kazu013.microcms.io/api/v1/blog/${slug}${
+    draftKey !== undefined ? `?draftKey=${draftKey}` : ''
+    }`,
+    { headers: { 'X-API-KEY': process.env.apiKey || '' } }
+  )
+  .then((res) => res.json());
+  return {
+    props: {
+      content
+    }
+  };
+};
