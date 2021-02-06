257247a67dfbf33b15e702a1c6c1db3349f5ebf1
karukan013 <karukan029@gmail.com>
2021-02-06 22:48:06 +0900

    Add Preview

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -12,13 +12,13 @@ export default function BlogId({ blog }) {
     </main>
   );
 }
-  
+
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
     const key = {
       headers: {'X-API-KEY': process.env.API_KEY},
     };
-    const data = await fetch('https://kazu013.microcms.io/api/v1/blog', key)
+    const data = await fetch(`${process.env.END_POINT}blog`, key)
       .then(res => res.json())
       .catch(() => null);
     const paths = data.contents.map(content => `/blog/${content.id}`);
@@ -31,8 +31,12 @@ export const getStaticPaths = async () => {
     const key = {
       headers: {'X-API-KEY': process.env.API_KEY},
     };
+    let url = `${process.env.END_POINT}blog/${id}`;
+    if (context.preview){
+      url += `?draftKey=${context.previewData.draftKey}`; 
+    }
     const data = await fetch(
-      'https://kazu013.microcms.io/api/v1/blog/' + id, key
+      `${process.env.END_POINT}blog/${id}`, key
     )
     .then(res => res.json())
     .catch(() => null);
