9d9a3d4c2161f2d1189c7c674e2d887cce86607f
karukan029 <karukan029@gmail.com>
2022-04-10 14:47:50 +0900

    chore: eslint周りの設定を修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,8 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
+    browser: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    project: './tsconfig.eslint.json',
-    sourceType: 'module',
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ['react', '@typescript-eslint'],
   extends: [
     'plugin:react/recommended',
     'airbnb',
@@ -22,14 +10,55 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      typescript: { project: './' },
+    },
+  },
   rules: {
     'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/destructuring-assignment': ['off'],
+    '@typescript-eslint/no-use-before-define': ['off'],
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'object-curly-newline': [
+      'error',
+      {
+        ImportDeclaration: 'never',
+      },
+    ],
     'react/jsx-props-no-spreading': [
       'error',
       { html: 'enforce', custom: 'ignore', explicitSpread: 'ignore' },
     ],
-    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
-    overrides: [{ files: ['*.tsx'], rules: { 'react/prop-types': 'off' } }],
+    'implicit-arrow-linebreak': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
+  overrides: [
+    {
+      files: ['*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
 };
