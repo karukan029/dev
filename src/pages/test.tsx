a7914d2013d9350c1523546536ca805fb7a0e664
karukan029 <karukan029@gmail.com>
2022-04-14 06:28:41 +0900

    fix: testコンポーネントをpagesから削除

--- src/pages/test.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import { CricleImage } from 'src/components/atoms';
-
-const TestComponent: FC = () => (
-  <CricleImage src="/twitter_zmem8eXI_200x200.jpg" diameter="200" />
-);
-
-export default TestComponent;
