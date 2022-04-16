07fa2c6402c5af904aa7d4425035fe4fb7e1db35
karukan029 <karukan029@gmail.com>
2022-04-16 10:17:27 +0900

    fix: ssgでスタイルが適用されない問題を修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-const Document = () => (
-  <Html lang="ja">
-    <Head>
-      <meta charSet="utf-8" />
-    </Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-);
+// スタイルが初期表示時に適用されない対策(公式のコピー)
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-export default Document;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
