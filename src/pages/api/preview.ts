1a5a26de4628d66c6744274ade18e83ea09a583d
karukan029 <karukan029@gmail.com>
2022-04-16 10:54:45 +0900

    fix: プレビュー機能をaspida実装に合わせて修正

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,37 +1,62 @@
 /* eslint-disable consistent-return */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import fetch from 'node-fetch';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const preview = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (!req.query.id) {
-    return res.status(404).end();
-  }
-  // draftKeyが下書きであることを示す
-  const content = await fetch(
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    `${process.env.END_POINT}/blog/${req.query.id}?draftKey=${req.query.draftKey}`,
-    { headers: { 'X-API-KEY': process.env.API_KEY || '' } },
-  )
-    .then((response) => response.json())
-    .catch((error) => error);
+/** utils */
+import { client } from 'src/utils/api';
+import { toStringId } from 'src/utils/toStringId';
 
-  if (!content) {
-    return res.status(401).json({ message: 'Invalid slug' });
-  }
-  // res.setPreviewData()に渡した引数がgetStaticPropsの引数であるcontextから受け取れる
-  res.setPreviewData({
-    slug: content.id,
-    draftKey: req.query.draftKey,
+const preview = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> => {
+  // draftKeyが下書きであることを示す
+  const id = toStringId(req.query.id);
+
+  const draftKey = toStringId(req.query.draftKey);
+  // eslint-disable-next-line no-underscore-dangle
+  const post = await client.v1.blog._id(id).$get({
+    query: {
+      fields: 'title,image,category,body,publishedAt',
+      draftKey,
+    },
   });
+
+  if (!post) {
+    return res.status(401).json({ message: 'Invalid contentId' });
+  }
+
+  // res.setPreviewData()に渡した引数がgetStaticPropsの引数であるcontextから受け取れる
+  res.setPreviewData({ ...post, draftKey });
   // 本来の記事のパスにリダイレクト
-  res.writeHead(307, { Location: `/blog/${content.id}` });
-  res.end('Preview mode enabled');
+  res.writeHead(307, { Location: `/blog/${post.id}` });
+  res.end();
 };
+
+// // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// const preview = async (req: NextApiRequest, res: NextApiResponse) => {
+//   if (!req.query.id) {
+//     return res.status(404).end();
+//   }
+//   // draftKeyが下書きであることを示す
+//   const content = await fetch(
+//     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+//     `${process.env.END_POINT}/blog/${req.query.id}?draftKey=${req.query.draftKey}`,
+//     { headers: { 'X-API-KEY': process.env.API_KEY || '' } },
+//   )
+//     .then((response) => response.json())
+//     .catch((error) => error);
+
+//   if (!content) {
+//     return res.status(401).json({ message: 'Invalid slug' });
+//   }
+//   // res.setPreviewData()に渡した引数がgetStaticPropsの引数であるcontextから受け取れる
+//   res.setPreviewData({
+//     slug: content.id,
+//     draftKey: req.query.draftKey,
+//   });
+//   // 本来の記事のパスにリダイレクト
+//   res.writeHead(307, { Location: `/blog/${content.id}` });
+//   res.end('Preview mode enabled');
+// };
 
 export default preview;
