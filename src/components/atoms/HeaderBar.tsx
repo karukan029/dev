554fa718661a12dbf508d81a98beb3ff48c2aba9
karukan029 <karukan029@gmail.com>
2022-04-17 20:35:36 +0900

    fix: headerのレイアウトを修正

--- src/components/atoms/HeaderBar.tsx
+++ src/components/atoms/HeaderBar.tsx
@@ -7,18 +7,30 @@ type Props = {
 };
 
 const HeaderBar: FC<Props> = (props) => (
-  <Header className={props.className}>{props.children}</Header>
+  <Header className={props.className}>
+    <HeaderContent>{props.children}</HeaderContent>
+  </Header>
 );
 
 const Header = styled.header`
   width: 100%;
   height: 64px;
-  padding: 0 16px;
   background-color: ${(props) => props.theme.colors.primary.main};
   ${media.handheld426`
     height: 80px;
-    padding: 0 32px;
   `}
+`;
+
+const HeaderContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1140px;
+  padding: 0 16px;
+  margin: 0 auto;
+  ${media.handheld426`
+  padding: 0 36px;
+`}
 `;
 
 export default HeaderBar;
