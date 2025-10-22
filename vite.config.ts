854ceb79bd12ce3589da51d3458fc16db5cfa01a
karukan029 <karukan029@gmail.com>
2025-10-22 23:55:31 +0900

    chore: add biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,31 @@
-import build from '@hono/vite-build/cloudflare-workers'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import tailwindcss from '@tailwindcss/vite'
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
-import ssg from '@hono/vite-ssg'
+import build from "@hono/vite-build/cloudflare-workers";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+import honox from "honox/vite";
+import { defineConfig } from "vite";
+import ssg from "@hono/vite-ssg";
 
-const entry = './app/server.ts'
+const entry = "./app/server.ts";
 
 export default defineConfig({
-  plugins: [
-    honox({
-      devServer: { adapter },
-      client: { input: ['/app/client.ts', '/app/style.css'] }
-    }),
-    tailwindcss(),
-    build(),
-  ],
-  server: {
-    port: 3000,
-  },
-  ssr: {
-    external: ['rehype-sanitize', 'rehype-stringify', 'remark-parse', 'remark-rehype', 'unified']
-  },
-})
+	plugins: [
+		honox({
+			devServer: { adapter },
+			client: { input: ["/app/client.ts", "/app/style.css"] },
+		}),
+		tailwindcss(),
+		build(),
+	],
+	server: {
+		port: 3000,
+	},
+	ssr: {
+		external: [
+			"rehype-sanitize",
+			"rehype-stringify",
+			"remark-parse",
+			"remark-rehype",
+			"unified",
+		],
+	},
+});
