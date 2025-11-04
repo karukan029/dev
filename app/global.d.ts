e7339d7ceed9f30dbff62c40dd2c5febafc35e1e
karukan029 <karukan029@gmail.com>
2025-11-04 18:35:03 +0900

    chore: add process env type

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,2 +1,20 @@
 // Environment variables are accessed via process.env
 // Thanks to nodejs_compat_populate_process_env flag in wrangler.jsonc
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			/**
+			 * CMS API endpoint URL
+			 * @example "http://localhost:5173"
+			 */
+			CMS_URL: string;
+			/**
+			 * API key for CMS read access
+			 */
+			READ_API_KEY: string;
+		}
+	}
+}
+
+export {};
