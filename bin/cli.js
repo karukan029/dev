a30553b7ca4bd8ef695d64082af28e2401eed493
karukan029 <karukan029@gmail.com>
2025-12-30 15:26:58 +0900

    experimental: change node modlule ref

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 import { spawn } from "node:child_process";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
-import { wrangler } from "wrangler";
+import wrangler from "wrangler";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
